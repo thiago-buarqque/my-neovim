b64e8b7cb887d4dc21e7a59f2c7689677930e04f
Thiago Buarque <thiago.buarqque@gmail.com>
2025-02-16 15:32:28 -0300

    more mappings and better rust support

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,31 +19,38 @@ map("i", "<leader>e", "<ESC>")
 map("n", "<M-p>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 map("n", "<M-[>", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
 
+-- Line insertion
+map('n', '<leader>o', ':put _<CR>', { noremap = true, silent = true, desc = "Add line below without insert"})
+map('n', '<leader>O', ':put! _<CR>', { noremap = true, silent = true, desc = "Add line above without insert"})
 -- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+map("n", "<C-n>", "<cmd>enew<CR>", { desc = "buffer new" })
 
-map("n", "<C-tab>", function()
+-- Map Tab to indent the current line or selected lines in visual mode
+map("n", "<Tab>", ">>", { noremap = true, silent = true, desc = "Add tab" })
+map("n", "<S-Tab>", "<<", { noremap = true, silent = true, desc = "Remove tab" })
+
+map("n", "<C-l>", function()
   require("nvchad.tabufline").next()
 end, { desc = "buffer goto next" })
 
-map("n", "<C-S-tab>", function()
+map("n", "<C-h>", function()
   require("nvchad.tabufline").prev()
 end, { desc = "buffer goto prev" })
 
-map("n", "<leader>w", function()
+map("n", "<C-w>", function()
   require("nvchad.tabufline").close_buffer()
 end, { desc = "buffer close" })
 
 -- whichkey
 map("n", "<leader>K", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 
-map("n", "<leader>k", function()
-  vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
-end, { desc = "whichkey query lookup" })
+-- map("n", "<leader>k", function()
+--   vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+-- end, { desc = "whichkey query lookup" })
 
 -- Navigate back and forward
-vim.keymap.set('n', '<M-Left>', '<C-o>', { desc = 'Go back in jump list' })
-vim.keymap.set('n', '<M-Right>', '<C-i>', { desc = 'Go forward in jump list' })
+map('n', '<M-Left>', '<C-o>', { desc = 'Go back in jump list' })
+map('n', '<M-Right>', '<C-i>', { desc = 'Go forward in jump list' })
 
 -- Automatically remove old jumps from list
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -60,4 +67,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
 })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,7 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  {
+	  {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
@@ -19,6 +19,14 @@ return {
         file_ignore_patterns = { "**/node_modules", ".git", "**/build", "dist", "**/__pycache__", "**/target" },
       })
 
+      local telescopeBuiltin = require('telescope.builtin')
+
+      local map = vim.keymap.set
+
+      map('n', '<leader>F', telescopeBuiltin.live_grep, { desc = 'Telescope live grep' })
+      map('v', '<leader>F', telescopeBuiltin.grep_string, { desc = 'Find selection in workspace' })
+      map('n', '<leader>jp', telescopeBuiltin.jumplist, { desc = 'Show jumplist' })
+
       return conf
     end,
   },
@@ -26,5 +34,80 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "java", "rust", "css", "typescript", "properties", "json" } },
+  },
+
+  -- Rust stuff
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false,   -- This plugin is already lazy
+    ft = "rust",
+    config = function()
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
+    end,
+  },
+
+  {
+    'saecki/crates.nvim',
+    ft = { "toml" },
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" } }
+      })
+    end
   },
 }
